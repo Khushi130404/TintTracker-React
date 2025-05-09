f6b35b20aa2d521813a6e7848b0106b1b883b173
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-09 09:25:19 +0530

    Palette

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -6,13 +6,11 @@ const MainScreen = () => {
     <div className={styles.mainScreen}>
       <div className={styles.header}>Tint Tracker</div>
       <div className={styles.container}>
-        {/* <h1 className={styles.title}>
-          ✨ Welcome to Your Tint Tracker Dashboard ✨
-        </h1> */}
         <p className={styles.welcome}>
           Track your tints, monitor shades, and manage your palette
           effortlessly.
         </p>
+        
       </div>
     </div>
   );
