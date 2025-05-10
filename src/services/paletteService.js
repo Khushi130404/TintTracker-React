f6c1632807f78e7669eece7b8a33c466a6e8c0d4
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-10 09:53:02 +0530

    Show Palettes

--- src/services/paletteService.js
+++ src/services/paletteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/palette"; // Backend URL
+const API_BASE_URL = "http://localhost:3000/palette";
 
 export const insertPalette = async (paletteData) => {
   const response = await axios.post(`${API_BASE_URL}/insert`, paletteData);
